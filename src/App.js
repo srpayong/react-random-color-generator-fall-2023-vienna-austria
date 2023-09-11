f435291b4a26239256e45e3c85d621d5f8140a78
Sharmaine Payong <sharmainerpayong@gmail.com>
2023-09-11 22:15:24 +0200

    Your branch is up to date with 'origin/main'.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import './App.css';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 
-export default function App() {
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[randomNumber(16)];
+  }
+  return color;
+}
+
+function randomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function RandomColorGenerator() {
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
+
+  const changeColor = () => {
+    const randomColor = getRandomColor();
+    setBackgroundColor(randomColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Random Color Generator</h1>
+      <div
+        style={{
+          backgroundColor: backgroundColor,
+          width: '300px',
+          height: '300px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '24px',
+          color: 'white',
+        }}
+      >
+        Generated Color: {backgroundColor}
+      </div>
+      <button
+        onClick={changeColor}
+        style={{
+          padding: '10px',
+          fontSize: '16px',
+          backgroundColor: '#0074D9',
+          color: 'white',
+          border: 'none',
+          cursor: 'pointer',
+          marginTop: '10px',
+        }}
+      >
+        Generate Random Color
+      </button>
     </div>
   );
 }
+
+export default RandomColorGenerator;
