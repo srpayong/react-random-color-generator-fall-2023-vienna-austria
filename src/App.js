c5ec574d0b2d09d9e3d6ce273f6b3dd68bda598e
Sharmaine Payong <sharmainerpayong@gmail.com>
2023-09-13 22:59:01 +0200

    Edit colors

--- src/App.js
+++ src/App.js
@@ -1,103 +1,120 @@
-import React, { useState } from 'react';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
-// Generate a random color with optional hue and luminosity
-function generateRandomColor(hue = -1, luminosity = -1) {
-  // Generate a random hue if not provided
-  const randomHue = hue === -1 ? Math.floor(Math.random() * 360) : hue;
+export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState(randomColor());
+  const [hue, setHue] = useState(0); // Initialize hue value
+  const [lightness, setLightness] = useState(50); // Initialize lightness value
 
-  // Generate a random luminosity if not provided
-  const randomLuminosity =
-    luminosity === -1 ? Math.floor(Math.random() * 101) : luminosity;
-
-  return `hsl(${randomHue}, 100%, ${randomLuminosity}%)`;
-}
-
-function App() {
-  const [backgroundColor, setBackgroundColor] = useState(generateRandomColor());
-  const [hue, setHue] = useState(-1);
-  const [luminosity, setLuminosity] = useState(-1);
-
-  // Function to update color when sliders change
-  const updateColor = () => {
-    const color = generateRandomColor(hue, luminosity);
-    setBackgroundColor(color);
-  };
-
-  // Update the color when the luminosity slider changes
-  const handleLuminosityChange = (e) => {
-    const newLuminosity = e.target.value === '' ? -1 : parseInt(e.target.value);
-    setLuminosity(newLuminosity);
-    updateColor();
-  };
-
-  // Generate a random color when the button is clicked
-  const handleGenerateClick = () => {
-    updateColor();
+  const generateRandomColor = () => {
+    const newColor = randomColor({
+      hue: hue,
+      luminosity: 'light', // We use 'light' to control lightness
+    });
+    setBackgroundColor(newColor);
   };
 
   return (
-    <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1 style={{ color: 'pink', textDecoration: 'underline' }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 'thick',
+        flexDirection: 'column',
+        fontFamily: 'monospace',
+      }}
+    >
+      <h1
+        style={{
+          color: 'honeydew',
+          fontFamily: 'chalkduster',
+          textDecoration: 'underline dotted',
+        }}
+      >
         Random Color Generator
       </h1>
       <div
         style={{
-          backgroundColor: backgroundColor,
           width: '300px',
-          height: '150px',
+          height: '300px',
+          borderRadius: '200px',
+          backgroundColor: backgroundColor,
+          borderColor: 'white',
+          textAlign: 'center',
+          fontSize: '20px',
           display: 'flex',
-          alignItems: 'center',
           justifyContent: 'center',
-          fontSize: '24px',
-          color: 'white',
-          margin: '20px auto',
+          alignItems: 'center',
+          transition: 'ease-n-out',
         }}
       >
-        Generated Color: {backgroundColor}
+        Generated color: {backgroundColor}
+        <br />
       </div>
-      <button onClick={handleGenerateClick}>Generate</button>
-      <div
+      <br />
+      <button
         style={{
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          margin: '20px auto',
+          width: '15vw',
+          height: '8vh',
+          borderRadius: '10px',
+          fontSize: '20px',
+          background: 'backgroundColor',
+          color: 'black',
+          borderColor: 'backgroundColor',
+          borderWidth: '4px',
         }}
+        onClick={generateRandomColor}
       >
-        <label style={{ marginRight: '20px' }}>
-          Hue:
-          <br />
-          <input
-            type="range"
-            min="0"
-            max="360"
-            step="1"
-            value={hue === -1 ? '' : hue}
-            onChange={(e) => {
-              const newHue =
-                e.target.value === '' ? -1 : parseInt(e.target.value);
-              setHue(newHue);
-              updateColor();
-            }}
-          />
-          {hue !== -1 && <span>{hue}°</span>}
-        </label>
-        <label>
-          Luminosity:
-          <br />
-          <input
-            type="range"
-            min="0"
-            max="100"
-            step="1"
-            value={luminosity === -1 ? '' : luminosity}
-            onChange={handleLuminosityChange}
-          />
-          {luminosity !== -1 && <span>{luminosity}%</span>}
-        </label>
-      </div>
+        Generate
+      </button>
+      <br />
+
+      <h2>Hue</h2>
+      <input
+        type="range"
+        min="0"
+        max="360"
+        step="1"
+        value={hue}
+        onChange={(event) => setHue(parseInt(event.target.value))}
+        style={{
+          background: 'white',
+          borderColor: backgroundColor,
+          borderWidth: '3px',
+          width: '15vh',
+          height: '8vh',
+          borderRadius: '20px',
+          textAlign: 'center',
+          fontSize: '20px',
+        }}
+      />
+      <span>{hue}°</span>
+      <br />
+
+      <h2>Lightness</h2>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        step="1"
+        value={lightness}
+        onChange={(event) => setLightness(parseInt(event.target.value))}
+        style={{
+          background: 'white',
+          borderColor: backgroundColor,
+          borderWidth: '3px',
+          width: '15vh',
+          height: '8vh',
+          borderRadius: '20px',
+          textAlign: 'center',
+          fontSize: '20px',
+        }}
+      />
+      <span>{lightness}%</span>
     </div>
   );
 }
-
-export default App;
